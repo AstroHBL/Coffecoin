ea9132274fed761200f12348458ed7c1f2396f36
Astro_ <87935902+AstroHBL@users.noreply.github.com>
2022-07-26 13:13:23 +0200

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Teacoin is ERC20, ERC20Burnable, Ownable {
+contract Coffecoin is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("Coffecoin", "COFFEE") {
         _mint(msg.sender, 10000000000000 * 10 ** decimals());
     }
